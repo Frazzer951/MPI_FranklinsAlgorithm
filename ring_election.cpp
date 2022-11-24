2ada642d02544fab10c41e72d169e35fec6eabf4
Luke Eltiste <luke343279@gmail.com>
2022-11-23 17:15:58 -0800

    reorder send and receive to always work

--- ring_election.cpp
+++ ring_election.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <mpi.h>
 
+int genIdentifier(int rank, int size)
+{
+    int *nums;
+    nums = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        nums[i] = rand();
+    }
+
+    return nums[rank];
+}
+
 int main(int argc, char *argv[])
 {
     int rank, size;
@@ -8,13 +21,10 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    int id = genIdentifier(rank, size); // get random id
     bool is_active = true;
-    bool is_leader = false;
-    bool running = true;
 
-    int q[1] = {0};
-    int r[1] = {0};
-    int send[1] = {rank};
+    int q, r;
 
     int left = rank - 1;
     int right = (rank + 1) % size;
@@ -22,66 +32,49 @@ int main(int argc, char *argv[])
     if (rank == 0)
         left = size - 1;
 
-    while (running)
+    while (true)
     {
         if (is_active)
         {
-            std::cout << rank << " is sending to " << left << std::endl;
-            MPI_Send(&send, 1, MPI_INT, left, 0, MPI_COMM_WORLD);
-            MPI_Recv(&r, 1, MPI_INT, right, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            std::cout << rank << " received from " << right << std::endl;
-
-            std::cout << rank << " is sending to " << right << std::endl;
-            MPI_Send(&send, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
+            // std::cout << rank << " is sending to " << right << std::endl;
+            MPI_Send(&id, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
             MPI_Recv(&q, 1, MPI_INT, left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            std::cout << rank << " received from " << left << std::endl;
+            // std::cout << rank << " received from " << left << std::endl;
 
-            int max_v = (q[0] < r[0]) ? r[0] : q[0];
-            if (max_v > rank)
+            // std::cout << rank << " is sending to " << left << std::endl;
+            MPI_Send(&id, 1, MPI_INT, left, 0, MPI_COMM_WORLD);
+            MPI_Recv(&r, 1, MPI_INT, right, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // std::cout << rank << " received from " << right << std::endl;
+
+            int max_v = std::max(q, r);
+            if (max_v > id)
             {
-                std::cout << rank << " is now passive" << std::endl;
+                std::cout << rank << " is now passive with id: " << id << std::endl;
                 is_active = false;
             }
-            else if (max_v == rank)
+            else if (max_v == id)
             {
-                std::cout << rank << " is now the leader" << std::endl;
-                is_leader = true;
-                running = false;
+                std::cout << rank << " is now the leader with id: " << id << std::endl;
+                break;
             }
             else
             {
                 std::cout << rank << " continues to next round" << std::endl;
             }
         }
-        else
+        else // passive
         {
             MPI_Recv(&q, 1, MPI_INT, left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            std::cout << rank << " received from " << left << " passive" << std::endl;
-            std::cout << rank << " is relaying to " << right << std::endl;
+            // std::cout << rank << " is relaying to " << right << std::endl;
             MPI_Send(&q, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
 
             MPI_Recv(&r, 1, MPI_INT, right, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            std::cout << rank << " received from " << right << " passive" << std::endl;
-            std::cout << rank << " is relaying to " << left << std::endl;
+            // std::cout << rank << " is relaying to " << left << std::endl;
             MPI_Send(&r, 1, MPI_INT, left, 0, MPI_COMM_WORLD);
-
-            if (q[0] == -1 or r[0] == -1)
-            {
-                // leader found
-                running = false;
-            }
         }
     }
 
-    if (is_leader)
-    {
-        std::cout << rank << " is sending leader message" << std::endl;
-        // Send message indicating leader was found
-        send[0] = -1;
-        MPI_Send(&send, 1, MPI_INT, left, 0, MPI_COMM_WORLD);
-        MPI_Send(&send, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
-    }
-
+    MPI_Abort(MPI_COMM_WORLD, MPI_SUCCESS); // Kill rest of processes since leader was found
     MPI_Finalize();
     return 0;
 }
